97bffaae6040d48718392342c3d4df03d5c68618
Zach Shute <cosmonot1@gmail.com>
2018-12-10 19:16:07 -0800

    oops

--- api/v0/specimen.js
+++ api/v0/specimen.js
@@ -3,6 +3,7 @@
 const flat = require( 'flat' );
 const Specimen = require( '../../models/v0/specimen' );
 const { callbackify: c, formatBody: fmtBody } = require( '../../utils/api' );
+const moment = require( 'moment' );
 
 module.exports = {
   add: c( fmtBody( fmtReqRes( add ) ) ),
@@ -18,6 +19,20 @@ async function add( data ) {
 
   data.specimen.species.additional = data.specimen.species.additional.split( ' ' );
 
+  try {
+    const date = moment( data.specimen.acquired.date  );
+    if ( !date.isValid() ) {
+      throw new Error( 'Bad date' );
+    }
+    data.specimen.acquired.date = date.toISOString();
+  } catch ( err ) {
+    throw {
+      code: 400,
+      reason: 'Unable to format acquired date provided.',
+      err: new Error( 'Unable to format acquired date provided.' )
+    }
+  }
+
   //TMP OP FOR TESTING BEFORE PHOTOS ARE IMPLEMENTED
   //TODO: REMOVE ONCE PHOTOS IMPLEMENTED
   data.specimen.photos = {
@@ -30,6 +45,10 @@ async function add( data ) {
 
 function fmtReqRes( fn ) {
   return async req => {
+
+    if ( !req.body.specimen ) {
+      req.body.specimen = {}
+    }
 
     req.body.specimen._id = req.params._id;
 
