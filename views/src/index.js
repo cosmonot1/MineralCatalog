19b913dda2442fa444974ce282fa1b7ffc4d270d
Zach-PC\Zach <cosmonot1@gmail.com>
2018-12-23 13:57:53 -0800

    debug

--- views/src/index.js
+++ views/src/index.js
@@ -1240,3 +1240,180 @@ ReactDOM.render(
 function capitalize( a ) {
   return a.charAt( 0 ).toUpperCase() + a.slice( 1 );
 }
+
+window.API = ( function API() {
+
+  // Detect browser support for CORS
+  if ( !( 'withCredentials' in new XMLHttpRequest() ) && typeof XDomainRequest === 'undefined' ) {
+    throw new Error( 'CORS not supported by browser.' );
+  }
+
+  const that = Object.create( API.prototype );
+
+  that.version = '0.0.1';
+
+  const isRelativeUrl = url => {
+    try {
+      new URL( url );
+    }
+    catch ( err ) {
+      return true;
+    }
+  };
+
+  const ajax = ( method, url, data, done ) => {
+
+    const cookie = window.getCookie( 'access_token' );
+
+    if ( method.toUpperCase() === 'GET' ) {
+      url += QueryString.encode( window.flatten( data || {} ) );
+    }
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open( method, url, true );
+    xhr.responseType = 'json';
+
+    if ( isRelativeUrl( url ) ) {
+      xhr.setRequestHeader( 'Content-Type', 'application/json;charset=UTF-8' );
+      xhr.setRequestHeader( 'Authorization', 'Bearer ' + cookie );
+    }
+
+    xhr.onload = () => {
+
+      if ( !xhr.response ) {
+        return done( new Error( 'Received invalid response from server.' ), null );
+      }
+
+      if ( !( xhr.response.err && ( xhr.response.err.code === 401 ) && cookie ) ) {
+        return done( xhr.response.err || null, xhr.response.err ? null : xhr.response );
+      }
+
+      alert( 'Authentication session token expired. Please refresh the page and log in again.' );
+
+      const query = {
+        redirect: window.location.href
+      };
+
+    };
+
+    if ( method.toUpperCase() === 'GET' ) {
+      return xhr.send();
+    }
+
+    xhr.send( JSON.stringify( data || {} ) );
+
+  };
+
+  that.user = {
+
+    authorize: ( data, done ) =>
+      ajax( 'POST', '/api/v0/authorize', data, done ),
+
+    logout: ( data, done ) =>
+      ajax( 'POST', '/api/v0/user/logout', data, done ),
+
+  };
+
+  that.specimen = {
+
+    add: ( data, done ) =>
+      ajax( 'POST', '/api/v0/specimen', data, done ),
+
+    get: ( data, done ) =>
+      ajax( 'GET', `/api/v0/specimen/${data.specimen._id}`, data, done ),
+
+    list: ( data, done ) =>
+      ajax( 'POST', '/api/v0/specimen/list', data, done ),
+
+    upload: ( data, done ) =>
+      ajax( 'GET', `/api/v0/specimen/upload/${data.type}`, data, done ),
+
+    remove: ( data, done ) =>
+      ajax( 'DELETE', '/api/v0/specimen', data, done ),
+
+    update: ( data, done ) =>
+      ajax( 'PATCH', `/api/v0/specimen/${data.specimen._id}`, data, done )
+
+  };
+
+  return that;
+
+} )();
+
+window.getCookie = name => {
+
+  const value = `; ${document.cookie}`;
+  const parts = value.split( `; ${name}=` );
+
+  if ( parts.length === 2 ) {
+    return parts.pop().split( ';' ).shift();
+  }
+
+};
+
+window.setCookie = ( name, value ) => {
+  document.cookie = `${name}=${value}; secure; path=/;`;
+};
+
+window.deleteCookie = name => {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;`;
+};
+
+window.flatten = ( object, prefix = '' ) =>
+  Object.keys( object ).reduce(
+    ( prev, element ) =>
+      object[ element ] &&
+      typeof object[ element ] === 'object' &&
+      !Array.isArray( object[ element ] ) ?
+        { ...prev, ...flatten( object[ element ], `${prefix}${element}.` ) } :
+        { ...prev, ...{ [ `${prefix}${element}` ]: object[ element ] } },
+    {}
+  );
+
+class QueryString {
+
+  static encode( q ) {
+
+    let qs = Object.keys( q ).reduce( ( qs, k ) => {
+
+      if ( q[ k ] === undefined ) {
+        return qs;
+      }
+
+      const key = encodeURIComponent( k );
+      const val = encodeURIComponent( q[ k ] );
+
+      return qs + `&${key}=${val}`;
+
+    }, '' );
+
+    if ( qs ) {
+      qs = '?' + qs.slice( 1 );
+    }
+
+    return qs;
+
+  }
+
+  static decode( qs ) {
+
+    // Remove leading `?`
+    qs = qs.substring( 1 );
+
+    const pl = /\+/g;
+    const search = /([^&=]+)=?([^&]*)/g;
+    const decode = s => decodeURIComponent( s.replace( pl, ' ' ) );
+
+    let match;
+    const q = {};
+
+    while ( match = search.exec( qs ) ) {
+      q[ decode( match[ 1 ] ) ] = decode( match[ 2 ] );
+    }
+
+    return q;
+
+  }
+
+}
