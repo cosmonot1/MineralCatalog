c0e55777fba5367fb65b775f4dcc2939272a3b33
Zach Shute <cosmonot1@gmail.com>
2019-03-14 15:23:24 -0700

    docker file bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require( "path" );
 
 module.exports = {
-  entry: './views/src/index.js',
+  entry: path.resolve( __dirname, './views/src/index.js' ),
   module: {
     rules: [
       {
